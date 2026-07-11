373d8fe11fdadf8e4ae4c180287fe3a4a5b7e896
nilum2002 <sachithyanilum@gmail.com>
2026-07-11 18:07:07 +0530

    chore: untrack build and install folders and update gitignore

--- dr_spaam_ros2/build/dr_spaam_ros2/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/media/nilum/my-stuff/Research/Human_Robot_Interaction/2D_lidar_person_detection/dr_spaam_ros2/install/dr_spaam_ros2'
